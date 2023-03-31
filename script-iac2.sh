6dd6872dc48ec91f056629c99376d9db7374b205
vitortsouza <129435933+vitortsouza@users.noreply.github.com>
2023-03-31 18:41:32 -0300

    Update script-iac2.sh

--- script-iac2.sh
+++ script-iac2.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
+if [ "$(whoami)" !- "root" ]; then
+	echo "Necessario usuario root para instalar"
+	exit 1
+
 echo "Atualizando o servidor..."
-apt-get update
-apt-get upgrade -y
+apt-get update & upgrade -y
 apt-get install apache2 -y
 apt-get install unzip -y
 
-
 echo "Baixando e copiando os arquivos da aplicação..."
 
-cd /tmp
-wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
-unzip main.zip
+cd /tmp 
+wget https://github.com/vitortsouza/dio-linux-projeto2/raw/main/linux-site-dio-main.zip
+unzip linux-site-dio-main.zip
 cd linux-site-dio-main
 cp -R * /var/www/html/
-
-
-
